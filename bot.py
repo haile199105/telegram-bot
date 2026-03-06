28ef1a2ddcdce498bd587ca7a75e33eb815c42ea
Haile Shibru <129173186+haile199105@users.noreply.github.com>
2026-03-06 11:00:38 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,23 +1,21 @@
 import os
-import asyncio
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
 
 TOKEN = os.environ.get('TOKEN')
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def start(update: Update, context):
     await update.message.reply_text("Hello! Haile's bot is alive.")
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def echo(update: Update, context):
     await update.message.reply_text(update.message.text)
 
-async def main():
-    app = ApplicationBuilder().token(TOKEN).build()
+def main():
+    print("Bot is starting...")
+    app = Application.builder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-    
-    print("Bot is starting...")
-    await app.run_polling()
+    app.run_polling()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
