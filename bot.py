f2ed8242bacb0b2f2eb1b2fd68f8f3524278490e
Haile Shibru <129173186+haile199105@users.noreply.github.com>
2026-03-06 10:46:55 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 
-TOKEN = "7704863421:AAG1KipKPvq9nkLsuR1PPYqy6IUUIx9wj1o"
+TOKEN = os.environ.get('TOKEN')
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hello! Haile's bot is alive and working!")
+    await update.message.reply_text("Hello! Haile's bot is alive.")
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(update.message.text)
@@ -13,5 +14,4 @@ app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
-print("Bot is starting...")
 app.run_polling()
